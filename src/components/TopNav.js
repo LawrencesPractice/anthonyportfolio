3d4cb5bce041c8263ef64d9641df01ece54ab350
“Lawrence <tbby28@gmail.com>
2023-10-06 12:41:11 +1100

    name change

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -8,11 +8,11 @@ const TopNav = ({ links, setSelectedLink }) => {
   return (
     <nav className="top-nav">
       <div className="top-nav-left">
-        <span className="email">AnthonyNi@example.com</span>
+        <span className="email">Lawrence@example.com</span>
       </div>
       <div className="top-nav-middle">
         <div className="half-circle"></div> {/* Half circle element */}
-        <span className="brand">Anthony Ni</span>
+        <span className="brand">Lawrence Tsui Portfolio templete site</span>
       </div>
       <ul className="top-nav-links">
         {links.map((link, index) => (
